a6adfbc3b4d26f9b8dcf1d07638f7ff446eee674
LuytBui <luytbq43@gmail.com>
2024-09-30 00:13:41 +0700

    update to angular 18

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LoginComponent } from './components/login/login/login.component';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http';
-// import CookieService from 'ngx-cookie-service';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import CookieService from 'ngx-cookie-service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    bootstrap: [AppComponent],
+    imports: [AppRoutingModule,
+        BrowserModule,
+        ReactiveFormsModule],
+    providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        // CookieService
+    ]
 })
 export class AppModule { }
